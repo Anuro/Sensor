de5942cef9d93bf1ae12c0d6bd0b41be433a5ea2
Giss87 <giselle.valds@gmail.com>
2018-10-23 15:19:08 -0500

    I have done two things: I matched a pin of the pic with the sensor

--- Code/Temperature_sensor.c
+++ Code/Temperature_sensor.c
@@ -14,19 +14,28 @@
 #define LCD_DATA7 PIN_D7
 #include <lcd.c>
 
-lm35v = read_adc();                    // Here, the sensor reads the current temperature
-temp = lm35v*0.488;                    // We multiplicate what the sensor read with the number of mV that are in each bit (0.488)
-lcd_gotoxy(1,1);                       // We indicate where the cursor of the LCD will be
-printf(lcd_putc,"\f T = %f",temp);      // The pic shows us what is the current temperature via LCD
-if(temp>40)                                
+#define buzzer pin_a1 //we define the pin of the pic with respect to the sensor.
+int lm35v,cont=0;     
+float temp;
+ 
+#int_timer3
+void read_temperature()  // This is a function inside the timer3 interrupt which read the remperature and show it in the LCD
 {
-   printf(lcd_putc,"WARNING!!!");      //We print  WARNING!!! when the temperature is major that 40°C.
-   while(temp>40)                      //The thing that happen when the temperature is major 40°C.
-{
+    cont++;                                   // We made a accountant in order to read the temperature every second
+   if(cont==40)                              // 40*(25ms) = 1 second
+   {
+
+    	lm35v = read_adc();                    // Here, the sensor reads the current temperature
+		temp = lm35v*0.488;                    // We multiplicate what the sensor read with the number of mV that are in each bit (0.488)
+		lcd_gotoxy(1,1);                       // We indicate where the cursor of the LCD will be
+		printf(lcd_putc,"\f T = %f",temp);      // The pic shows us what is the current temperature via LCD
+		if(temp>40)                                
+		{
+   			printf(lcd_putc,"WARNING!!!");      //We print  WARNING!!! when the temperature is major that 40°C.
+   			while(temp>40)                      //The thing that happen when the temperature is major 40°C.
+			{
 
 
-  
-  
   
   set_timer3(15535);                        // The timer3 is set to work wit 25ms
    clear_interrupt(int_timer3);              // The timer3 is clean
